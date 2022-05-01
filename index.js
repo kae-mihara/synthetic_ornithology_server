933e1a1b425b7abff0f5783c2bd1bcba3b911a07
Frederick Rodrigues <fredaudio@gmail.com>
2022-05-01 19:57:37 +1000

    esr

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const Datastore = require('nedb');
 const fetch = require('node-fetch');
 const fs = require('fs');
 require('dotenv').config();
-
+var https = require("https");
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Starting server at ${port}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(3000, function () {
+    console.log(
+      "Example app listening on port 3000! Go to https://localhost:3000/"
+    );
+  });
 app.use(express.static(__dirname + '/dist'));
 app.use(express.json({ limit: '100mb' }));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
 
-app.get('/data', (request, response) => {
+app.get('/api', (request, response) => {
   database.find({}, (err, data) => {
     if (err) {
       response.end();
