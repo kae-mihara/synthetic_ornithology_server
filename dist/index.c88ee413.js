33353cbed9e2ad2610ace3605fa1cdf6931e16bd
Frederick Rodrigues <fredaudio@gmail.com>
2022-05-20 20:54:14 +1000

    moved database

--- dist/index.c88ee413.js
+++ dist/index.c88ee413.js
@@ -21,6 +21,21 @@ const latMinSL = document.getElementById("latMin");
 const latMaxSL = document.getElementById("latMax");
 const lonMinSL = document.getElementById("lonMin");
 const lonMaxSL = document.getElementById("lonMax");
+const mapFilters = document.getElementById("mapFilters");
+mapFilters.style.display = "none";
+const settingsButton = document.getElementById("settingsButton");
+settingsButton.addEventListener("click", showHideDiv);
+function showHideDiv() {
+    console.log("show div " + mapFilters.style.display);
+    if (mapFilters.style.display === "none") {
+        mapFilters.style.display = "inline-block";
+        disableMap();
+    } else {
+        mapFilters.style.display = "none";
+        getData();
+        enableMap();
+    }
+}
 var bounds = [
     [
         latMaxSL.value,
@@ -54,16 +69,24 @@ function resetBoundingBox() {
     mymap.addLayer(boundingBox);
 }
 latMinSL.addEventListener("change", function() {
+    document.getElementById("latMinL").textContent = "Min Latitude: " + latMinSL.value;
     resetBoundingBox();
+    getData();
 });
 latMaxSL.addEventListener("change", function() {
+    document.getElementById("latMaxL").textContent = "Max Latitude: " + latMaxSL.value;
     resetBoundingBox();
+    getData();
 });
 lonMinSL.addEventListener("change", function() {
+    document.getElementById("lonMinL").textContent = "Min Longitude: " + lonMinSL.value;
     resetBoundingBox();
+    getData();
 });
 lonMaxSL.addEventListener("change", function() {
+    document.getElementById("lonMaxL").textContent = "Max Longitude: " + lonMaxSL.value;
     resetBoundingBox();
+    getData();
 });
 const minTempSL = document.getElementById("minTemp");
 const maxTempSL = document.getElementById("maxTemp");
@@ -71,23 +94,21 @@ let minTemp = 0;
 let maxTemp = 40;
 minTempSL.addEventListener("change", function() {
     minTemp = minTempSL.value;
+    document.getElementById("minTempL").textContent = "Min temp: " + minTempSL.value;
     getData();
-    console.log("Min temp: " + minTemp);
+    minTempSL.setAttribute("label", minTempSL.value);
 });
 maxTempSL.addEventListener("change", function() {
     maxTemp = maxTempSL.value;
+    document.getElementById("maxTempL").textContent = "Max temp: " + maxTempSL.value;
     getData();
-    console.log("Max temp: " + maxTemp);
 });
-const earliestDate = document.getElementById("start");
+const earliestDate = document.getElementById("start-date");
 const latestDate = document.getElementById("end-date");
-earliestDate.max = new Date().toISOString().substring(0, 10);
-latestDate.value = new Date().toISOString().substring(0, 10);
-latestDate.max = new Date().toISOString().substring(0, 10);
 function isSampleInDateRange(dateStamp) {
     // format: mm.dd.yyyy;
-    var minDate = earliestDate.value.getTime();
-    var maxDate = latestDate.value.getTime();
+    var minDate = earliestDate.value;
+    var maxDate = latestDate.value;
     return minDate < dateStamp && dateStamp < maxDate;
 }
 getData();
@@ -101,20 +122,43 @@ async function getData() {
     for (item of data){
         console.log("Getting data : " + counter + " " + item);
         counter++;
+        console.log(latMinSL.value + " " + item.coord.lat + " " + latMaxSL.value + " " + lonMinSL.value + " " + item.coord.lon + " " + lonMaxSL.value);
         // if(item.coord.lat > latMinSL.value && item.coord.lat < latMaxSL.value){
         //console.log("Min temp: " + minTemp + " Max Temp: " + maxTemp + " Actual temp: " + item.main.temp)
         if (item.main.temp > minTemp && item.main.temp < maxTemp) {
-            const marker = L.marker([
-                item.coord.lat,
-                item.coord.lon
-            ]);
-            marker.bindPopup("Location:" + item.name + "<br/> Weather :" + item.weather[0].description + "<br/> Date:" + item.dateName + "<br/> Temperature :" + item.main.temp + '&deg; C.<br/><br/><audio controls><source src="/audiofiles/' + item.timeStamp + '.wav"> type="audio/wave" </audio>');
-            markers.addLayer(marker);
+            if (item.coord.lat > latMinSL.value && item.coord.lat < latMaxSL.value) {
+                const marker = L.marker([
+                    item.coord.lat,
+                    item.coord.lon
+                ]);
+                marker.bindPopup("Location:" + item.name + "<br/> Weather :" + item.weather[0].description + "<br/> Date:" + item.dateName + "<br/> Temperature :" + item.main.temp + '&deg; C.<br/><br/><audio controls><source src="/audiofiles/' + item.timeStamp + '.wav"> type="audio/wave" </audio>');
+                markers.addLayer(marker);
+            }
         }
     //  }
     }
-    console.log(data);
+    //console.log(data);
     mymap.addLayer(markers);
+}
+function disableMap() {
+    mymap.dragging.disable();
+    mymap.touchZoom.disable();
+    mymap.doubleClickZoom.disable();
+    mymap.scrollWheelZoom.disable();
+    mymap.boxZoom.disable();
+    mymap.keyboard.disable();
+    if (mymap.tap) mymap.tap.disable();
+    document.getElementById("checkinMap").style.cursor = "default";
+}
+function enableMap() {
+    mymap.dragging.enable();
+    mymap.touchZoom.enable();
+    mymap.doubleClickZoom.enable();
+    mymap.scrollWheelZoom.enable();
+    mymap.boxZoom.enable();
+    mymap.keyboard.enable();
+    if (mymap.tap) mymap.tap.enable();
+    document.getElementById("checkinMap").style.cursor = "grab";
 }
 
 //# sourceMappingURL=index.c88ee413.js.map
