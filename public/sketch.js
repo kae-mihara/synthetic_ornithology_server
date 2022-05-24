9f5930eb5f33d2f308eef855e2788097ebaf9e48
Frederick Rodrigues <fredaudio@gmail.com>
2022-05-24 13:55:22 +1000

    sliders with ranges

--- public/sketch.js
+++ public/sketch.js
@@ -22,6 +22,13 @@ googleTerrain = L.tileLayer(
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(mymap);
 
+var minTemp = 0;
+var maxTemp = 0;
+var latMin = -20;
+var latMax = 20;
+var lonMin = -20;
+var lonMax = 20;
+
 const latMinSL = document.getElementById("latMin");
 const latMaxSL = document.getElementById("latMax");
 const lonMinSL = document.getElementById("lonMin");
@@ -39,86 +46,39 @@ function showHideDiv() {
     disableMap();
   } else {
     mapFilters.style.display = "none";
+
     getData();
     enableMap();
   }
 }
 var bounds = [
-  [latMaxSL.value, lonMaxSL.value],
-  [latMinSL.value, lonMinSL.value],
+  [latMin, lonMin],
+  [latMax, lonMax],
 ];
 var boundingBox = L.rectangle(bounds, { color: "#ff7800", weight: 1 });
 
 function resetBoundingBox() {
   mymap.removeLayer(boundingBox);
   bounds = [
-    [latMinSL.value, lonMinSL.value],
-    [latMaxSL.value, lonMaxSL.value],
+    [latMin, lonMin],
+    [latMax, lonMax],
   ];
   boundingBox = L.rectangle(bounds, { color: "#ff7800", weight: 1 });
   mymap.addLayer(boundingBox);
-  corner1 = L.latLng(latMinSL.value, lonMinSL.value);
-  corner2 = L.latLng(latMaxSL.value, lonMaxSL.value);
+  corner1 = L.latLng(latMin, lonMin);
+  corner2 = L.latLng(latMax, lonMax);
   geofilter = L.latLngBounds(corner1, corner2);
 }
 
-latMinSL.addEventListener("change", function () {
-  document.getElementById("latMinL").textContent =
-    "Min Latitude: " + latMinSL.value;
-  resetBoundingBox();
-  getData();
-});
 
-latMaxSL.addEventListener("change", function () {
-  document.getElementById("latMaxL").textContent =
-    "Max Latitude: " + latMaxSL.value;
-  resetBoundingBox();
-  getData();
-});
 
-lonMinSL.addEventListener("change", function () {
-  document.getElementById("lonMinL").textContent =
-    "Min Longitude: " + lonMinSL.value;
-  resetBoundingBox();
-  getData();
-});
-
-lonMaxSL.addEventListener("change", function () {
-  document.getElementById("lonMaxL").textContent =
-    "Max Longitude: " + lonMaxSL.value;
-  resetBoundingBox();
-  getData();
-});
-
-const minTempSL = document.getElementById("minTemp");
-const maxTempSL = document.getElementById("maxTemp");
-var minTemp = 0;
-var maxTemp = 40;
-
-minTempSL.addEventListener("change", function () {
-  minTemp = minTempSL.value;
-  document.getElementById("minTempL").textContent =
-    "Min temp: " + minTempSL.value;
-  getData();
-  minTempSL.setAttribute("label", minTempSL.value);
-});
-
-maxTempSL.addEventListener("change", function () {
-  maxTemp = maxTempSL.value;
-  document.getElementById("maxTempL").textContent =
-    "Max temp: " + maxTempSL.value;
-  getData();
-});
-
-const earliestDate = document.getElementById("start-date");
-const latestDate = document.getElementById("end-date");
 
 function isSampleInDateRange(dateStamp) {
   // format: mm.dd.yyyy;
-  var minDate = earliestDate.value;
-  var maxDate = latestDate.value;
+  // var minDate = earliestDate.value;
+  // var maxDate = latestDate.value;
 
-  return minDate < dateStamp && dateStamp < maxDate;
+  // return minDate < dateStamp && dateStamp < maxDate;
 }
 
 getData();
@@ -138,6 +98,7 @@ async function getData() {
         console.log("marker is in the range");
       }
       const marker = L.marker([item.coord.lat, item.coord.lon]);
+      marker.options.riseOnHover = true;
       marker.bindPopup(
         "Location:" +
           item.name +
@@ -178,3 +139,101 @@ function enableMap() {
   if (mymap.tap) mymap.tap.enable();
   document.getElementById("checkinMap").style.cursor = "grab";
 }
+
+var noUiSliderTempRange = document.getElementById("noUiSliderTempRange");
+
+noUiSlider.create(noUiSliderTempRange, {
+  start: [10, 40],
+  connect: true,
+  tooltips: [true, true],
+  range: {
+    min: -20,
+    max: 50,
+  },
+});
+
+noUiSliderTempRange.noUiSlider.on("update", function (values, handles) {
+  minTemp = values[0];
+  maxTemp = values[1];
+  getData();
+  console.log("Temp range: " + values);
+});
+
+var noUiSliderLongRange = document.getElementById("noUiSliderLongRange");
+
+noUiSlider.create(noUiSliderLongRange, {
+  start: [110, 155],
+  connect: true,
+  tooltips: [true, true],
+  range: {
+    min: 100,
+    max: 160,
+  },
+});
+
+noUiSliderLongRange.noUiSlider.on("update", function (values, handles) {
+  console.log("Long range: " + values);
+  lonMin = values[0];
+  lonMax = values[1];
+  resetBoundingBox();
+  getData();
+});
+
+var noUiSliderLatRange = document.getElementById("noUiSliderLatRange");
+
+noUiSlider.create(noUiSliderLatRange, {
+  start: [-45, -10],
+  connect: true,
+  tooltips: [true, true],
+  range: {
+    min: -50,
+    max: -8,
+  },
+});
+
+noUiSliderLatRange.noUiSlider.on("update", function (values, handles) {
+  console.log("Lat range: " + values);
+  latMin = values[0];
+  latMax = values[1];
+  resetBoundingBox();
+  getData();
+});
+
+
+function timestamp(str) {
+  return new Date(str).getTime();
+}
+
+var noUiSliderDateRange = document.getElementById("noUiSliderDateRange");
+
+var dateValues = [
+  document.getElementById('event-start'),
+  document.getElementById('event-end')
+];
+
+noUiSlider.create(noUiSliderDateRange, {
+  start: [timestamp('2019'), timestamp('2024')],
+  connect: true,
+  tooltips: [true, true],
+  handleAttributes: [
+    { "aria-label": "Earliest date" },
+    { "aria-label": "Latest date" },
+  ],
+  range: {
+    min: timestamp('2019'),
+    max: timestamp('2024')
+},
+});
+
+var formatter = new Intl.DateTimeFormat('en-GB', {
+  dateStyle: 'full'
+});
+
+
+
+noUiSliderDateRange.noUiSlider.on("update", function (values, handle) {
+  console.log("Date range: " + values);
+  dateValues[handle].innerHTML = formatter.format(new Date(+values[handle]));
+
+});
+
